a05a83b1fce245a07aa7bee78d668d11e0e734f1
till taeubrich <till.taeubrich@gmail.com>
2022-09-14 21:37:53 +1000

    finish exercise 1.12

--- 1.12-1.14/src/App.js
+++ 1.12-1.14/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const anecdotes = [
@@ -8,16 +8,22 @@ const App = () => {
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  ];
+
+  const selectNextAnecdote = () => {
+    const nextAnecdotePosition = Math.floor(Math.random() * anecdotes.length);
+    setSelected(nextAnecdotePosition);
+  };
+
+  const [selected, setSelected] = useState(0);
 
   return (
-    <div>
-      {anecdotes[selected]}
-    </div>
-  )
-}
+    <>
+      <div>{anecdotes[selected]}</div>
+      <button onClick={selectNextAnecdote}>next anecdote</button>
+    </>
+  );
+};
 
-export default App
+export default App;
